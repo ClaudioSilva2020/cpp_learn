e1c9ccb0b7d36de8f8f4f70d768eb42631d6838c
claudiosilva <claudioccdsilva@gmail.com>
2023-12-05 17:09:43 -0300

    Liskov principle

---
+++ design_patterns/SOLID/liskov/build/CMakeFiles/principle_liskov.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/principle_liskov.dir/src/main.cpp.o"
+  "CMakeFiles/principle_liskov.dir/src/main.cpp.o.d"
+  "principle_liskov"
+  "principle_liskov.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/principle_liskov.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
