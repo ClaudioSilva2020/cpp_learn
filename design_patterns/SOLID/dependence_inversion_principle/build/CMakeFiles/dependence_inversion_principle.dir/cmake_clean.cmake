6dfbe197797b400f3058f20f3d72ff4b4806927d
claudiosilva <claudioccdsilva@gmail.com>
2023-12-05 17:44:14 -0300

    dependence inversion principle

---
+++ design_patterns/SOLID/dependence_inversion_principle/build/CMakeFiles/dependence_inversion_principle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dependence_inversion_principle.dir/src/main.cpp.o"
+  "CMakeFiles/dependence_inversion_principle.dir/src/main.cpp.o.d"
+  "dependence_inversion_principle"
+  "dependence_inversion_principle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dependence_inversion_principle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
